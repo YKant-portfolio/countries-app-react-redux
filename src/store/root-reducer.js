f50c2e564f865b525f3e332564c14f9193ea4005
Yuriy <ykantakuzin@bk.ru>
2022-10-20 20:29:45 +0300

    first commit

---
+++ src/store/root-reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+// import { positionReducer } from "./positions/positions-reducer";
+
+export const rootReducer = combineReducers({
+	// positions: positionReducer,
+});
