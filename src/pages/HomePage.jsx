882e274c939a480b6bfee5dbf7f2ec5bb2c365a0
Yuriy <ykantakuzin@bk.ru>
2022-10-21 19:35:05 +0300

    added a reducer and rendering of countries in the user interface

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,46 +3,65 @@ import { useNavigate } from 'react-router-dom';
 import { List } from '../components/List';
 import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectAllCoutries, selectCoutriesInfo } from '../store/countries/countries-selector';
+import { loadCoutries } from '../store/countries/countries-actions';
 
 export const HomePage = () => {
 	const navigate = useNavigate();
 
-	const countries = [];
+	const dispatch = useDispatch();
+	const countries = useSelector(selectAllCoutries);
+	const { status, error, qty } = useSelector(selectCoutriesInfo);
+
+	useEffect(() => {
+		if (!qty) {
+			dispatch(loadCoutries());
+		}
+	}, [qty, dispatch]);
 
 	return (
 		<>
 			<Controls />
 
-			<List>
-				{countries.map((c) => {
-					const countryInfo = {
-						img: c.flags.png,
-						name: c.name,
-						info: [
-							{
-								title: 'Population',
-								description: c.population.toLocaleString(),
-							},
-							{
-								title: 'Region',
-								description: c.region,
-							},
-							{
-								title: 'Capital',
-								description: c.capital,
-							},
-						],
-					};
+			{error && <h2>Can't fetch data</h2>}
+			{status === 'loading' && <h2>loading...</h2>}
 
-					return (
-						<Card
-							key={c.name}
-							onClick={() => navigate(`/country/${c.name}`)}
-							{...countryInfo}
-						/>
-					);
-				})}
-			</List>
+			{status === 'received' && (
+				< List >
+					{
+						countries.map((c) => {
+							const countryInfo = {
+								img: c.flags.png,
+								name: c.name,
+								info: [
+									{
+										title: 'Population',
+										description: c.population.toLocaleString(),
+									},
+									{
+										title: 'Region',
+										description: c.region,
+									},
+									{
+										title: 'Capital',
+										description: c.capital,
+									},
+								],
+							};
+
+							return (
+								<Card
+									key={c.name}
+									onClick={() => navigate(`/country/${c.name}`)}
+									{...countryInfo}
+								/>
+							);
+						})
+					}
+				</List>
+			)}
 		</>
 	);
 };
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { themeReduser } from "./theme/theme-reducer";
+import { countriesReducer } from "./countries/countries-reducer";
 
 export const rootReducer = combineReducers({
 	theme: themeReduser,
+	countries: countriesReducer,
 });
